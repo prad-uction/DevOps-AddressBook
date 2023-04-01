b11da5761fb1c2d2f6a0f9e5a3e27b8f0d9e8258
kotagiriramachandra <97685796+kotagiriramachandra@users.noreply.github.com>
2023-03-31 20:39:31 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,54 +3,22 @@ def buildApp() {
             echo 'Inside the script'
             git 'https://github.com/kotagiriramachandra/DevOpsClassCodes.git' 
             echo 'After fetch'
-/*            post {
-                success {
-                    echo 'Build is successful'
-                }
-                failure {
-                    echo 'Build failed'
-                }
-            }*/
 }
+
 def compileApp() {
-            post {
-                success {
-                    echo 'Code compile is successful'
-                }
-                failure {
-                    echo 'Code compilation failed'
-                }
-            }
+            bat 'mvn compile'
 }
+
 def testApp() {
-            post {
-                success {
-                    echo 'Testing is successful'
-                }
-                failure {
-                    echo 'Testing failed'
-                }
-            }
+                bat 'mvn test'
 }
+
 def qaApp() {
-            post {
-                success {
-                    echo 'QA is successful'
-                }
-                failure {
-                    echo 'QA failed'
-                }
-            }
+                bat 'mvn pmd:pmd'
 }
+
 def pkgApp() {
-            post {
-                success {
-                    echo 'Package is successful'
-                }
-                failure {
-                    echo 'Package failed'
-                }
-            }
+                bat 'mvn package'
 }
 
 return this
